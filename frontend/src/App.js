1d368c4fd32647061eb609b1cb91fc8999d9b348
Ismail <ismail@gigel.id>
2018-07-30 00:51:35 +0700

    completing frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './component/navbar/NavBar';
+import Search from './component/search/Search';
+import Home from './component/home/Home';
+import Product from './component/product/Product';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; 
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={store} >
+        <BrowserRouter >
+          <MuiThemeProvider>
+            <div>
+              <NavBar />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/search' component={Search} />
+                <Route path='/product/:slug' component={Product}/>
+              </Switch>
+            </div>
+          </MuiThemeProvider> 
+        </BrowserRouter>  
+      </Provider>   
     );
   }
 }
