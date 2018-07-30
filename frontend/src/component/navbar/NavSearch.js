0917ab127ea3ea5f3c888cab706e14e7bf762d19
Ismail <ismail@gigel.id>
2018-07-30 08:16:34 +0700

    cleaning

--- frontend/src/component/navbar/NavSearch.js
+++ frontend/src/component/navbar/NavSearch.js
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from 'react-router-dom';
